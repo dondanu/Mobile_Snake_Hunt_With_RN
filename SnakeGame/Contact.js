8521c9ba6a96b7f85424881e8cc9aaf75b08fb08
Thamili | தமிழி <121654749+dondanu@users.noreply.github.com>
2025-03-25 06:41:20 +0530

    License

--- SnakeGame/Contact.js
+++ SnakeGame/Contact.js
@@ -1,15 +1,23 @@
-// Contact.js
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Contact = () => {
+const License = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Contact Information</Text>
-      <Text style={styles.info}>Snake Game Support</Text>
-      <Text style={styles.info}>Email: support@snakegame.com</Text>
-      <Text style={styles.info}>Phone: +1 234 567 8900</Text>
-      <Text style={styles.info}>Address: 123 Gaming Blvd, Fun City, GA</Text>
+      <Text style={styles.title}>License Information</Text>
+
+      {/* License Information */}
+      <Text style={styles.info}>License: Open Source (MIT License)</Text>
+      <Text style={styles.info}>Copyright: © 2025 Snake_Hunt</Text>
+      <Text style={styles.info}>Author: MR. S. K. DANUSAN</Text>
+      <Text style={styles.info}>Email: danusan.official@gmail.com</Text>
+      <Text style={{ color: 'white' }}> --------------------------------------</Text>
+
+      <Text style={styles.info}>This game is licensed under the MIT License.</Text>
+      <Text style={styles.info}>Feel free to modify, distribute, or use this game code for personal and commercial projects.</Text>
+      <Text style={styles.info}>For more details, please refer to the LICENSE file in the project repository.</Text>
+      <Text> </Text>
+      <Text style={{ color: 'white', fontSize: 20 }}> ~ All rights reserved</Text>
     </View>
   );
 };
@@ -19,18 +27,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black',
+    backgroundColor: '#282828',
+    padding: 20,
   },
   title: {
-    color: 'white',
-    fontSize: 24,
-    marginBottom: 20,
+    color: '#FF6347', // Tomato Red for title
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 15,
   },
   info: {
-    color: 'white',
+    color: '#00BFFF', // Deep Sky Blue for info text
     fontSize: 18,
     marginBottom: 10,
+    textAlign: 'center',
   }
 });
 
-export default Contact;
+export default License;
